90f811ef57072d1ab89d3eb8692d0469adc0e303
Emil Ståhl <emil.stahl@icloud.com>
2020-03-26 12:30:36 +0100

    Added arguments in main

---
+++ Projects/1. Hill Cipher/HillCipher.java
@@ -0,0 +1,20 @@
+
+public class HillCipher {
+
+    private static int MAX_RADIX = 256;
+    private static int MAX_BLOCKSIZE = 4;
+
+    public static void main(String[] args) {
+
+        int radix, blocksize;
+
+        if (args.length < 5) {
+            System.out.println("Usage: <radix> <blocksize> <keyfile> <plainfile> <cipherfile>");
+            System.exit(1);
+        }
+
+        radix = (Integer.parseInt(args[0]) <= MAX_RADIX) ? Integer.parseInt(args[0]) : MAX_RADIX;
+        blocksize = (Integer.parseInt(args[1]) <= MAX_BLOCKSIZE) ? Integer.parseInt(args[1]) : MAX_BLOCKSIZE;
+
+    }
+}
