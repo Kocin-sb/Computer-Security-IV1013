fd3a29eb1a6a4b1eadf2250351dbf7367a8a1b29
Emil Ståhl <emil.stahl@icloud.com>
2020-03-26 15:01:24 +0100

    works

--- Projects/1. Hill Cipher/HillCipher.java
+++ Projects/1. Hill Cipher/HillCipher.java
@@ -1,10 +1,27 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HillCipher {
 
     private static int MAX_RADIX = 256;
     private static int MAX_BLOCKSIZE = 4;
+    private static ArrayList<String> plain;
+    private static Scanner sc;
+    File file;
+
+    public static void readPlain(String fileName, int blocksize) {
+
+        try {
+            Scanner sc = new Scanner(new File(fileName));
+            plain = new ArrayList<String>();
+            while (sc.hasNext()) {
+                plain.add(sc.next());
+            }
+            sc.close();
+        } catch (FileNotFoundException ex) {
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -14,15 +31,12 @@ public class HillCipher {
             System.out.println("Usage: <radix> <blocksize> <keyfile> <plainfile> <cipherfile>");
             System.exit(1);
         }
-
         radix = (Integer.parseInt(args[0]) <= MAX_RADIX) ? Integer.parseInt(args[0]) : MAX_RADIX;
         blocksize = (Integer.parseInt(args[1]) <= MAX_BLOCKSIZE) ? Integer.parseInt(args[1]) : MAX_BLOCKSIZE;
 
-        File plainFile = new File(args[4]);
-        try {
-            Scanner sc = new Scanner(plainFile);
-        } catch (FileNotFoundException exception) {
-        }
+        readPlain(args[4], blocksize);
+
+        System.out.println(plain);
 
     }
 }
