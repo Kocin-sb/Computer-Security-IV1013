feb2ed5231f6f613dedc9dc13e630fafd9ef7776
Emil Ståhl <emil.stahl@icloud.com>
2020-03-29 14:11:11 +0200

    Create Hillkeys.java

---
+++ Projects/1. Hill Cipher/Hillkeys.java
@@ -0,0 +1,21 @@
+
+
+
+
+public class HillKeys {
+
+    public static String keyFile;
+    public static int radix, blocksize;
+   
+
+
+    public static void main(String args) {
+
+        
+
+
+    }
+
+
+
+}
