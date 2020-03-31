607e5b5cb1bd7f73a964599771304b8fce504c4b
Emil Ståhl <emil.stahl@icloud.com>
2020-03-31 17:52:06 +0200

    works with 8  letter word

--- Projects/1. Hill Cipher/scripts/testAll.sh
+++ Projects/1. Hill Cipher/scripts/testAll.sh
@@ -4,7 +4,7 @@ rm cipher
 rm plain
 echo "Generating key:"
 javac -cp ".:./jscience.jar" HillKeys.java
-java -cp ".:./jscience.jar" HillKeys 26 3 key
+java -cp ".:./jscience.jar" HillKeys 26 8 key
 
 echo "Key generated"
 cat key 
@@ -14,7 +14,7 @@ echo "Encrypting plain-alpha"
 
 echo "Cipher created" ; echo " "
 javac HillCipher.java
-java HillCipher 26 3 key plain-alpha cipher
+java HillCipher 26 8 key plain-alpha cipher
 
 cat cipher
 
@@ -23,7 +23,7 @@ echo " "
 echo "Decrypting cipher"
 
 javac -cp ".:./jscience.jar" HillDecipher.java
-java -cp ".:./jscience.jar" HillDecipher 26 3 key plain cipher
+java -cp ".:./jscience.jar" HillDecipher 26 8 key plain cipher
 
 echo "Cipher decrypted" ; echo " "
 
