45980f335e21b25564b7510705c2247265b5cc33
Emil Ståhl <emil.stahl@icloud.com>
2020-04-03 13:19:08 +0200

    read in of message

--- Projects/2. One-way Hash Function/CollisionResistance.java
+++ Projects/2. One-way Hash Function/CollisionResistance.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+
+
 public class CollisionResistance {
 
     //declare byte array for digest, inputBytes, tryDigest
@@ -17,19 +21,23 @@ public class CollisionResistance {
 
     }
 
-    public byte[] makeDigest(String inputText){
+    //public byte[] makeDigest(String inputText){
 
         // create object of message digest with SHA-256
         // create inputBytes of inputText with correct encoding
         // call object.update with inputBytes
         // create digest by object.digest();
 
-        return digest;
-    }
+       // return digest;
+   // }
 
     public static void main(String[] args) {
 
         // Read in message
+        System.out.println("Type message to digest:");
+        Scanner sc = new Scanner(System.in);
+        String msgToDigest = sc.nextLine();
+        System.out.println(msgToDigest);
         // Make digest by calling function makeDigest
         //Print digest
         //Perform brute force with digest
