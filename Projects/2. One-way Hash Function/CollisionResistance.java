47e63edff2c97c51a673c8a364af39a4da6236c0
Emil Ståhl <emil.stahl@icloud.com>
2020-04-06 13:58:05 +0200

    making of digest work

--- Projects/2. One-way Hash Function/CollisionResistance.java
+++ Projects/2. One-way Hash Function/CollisionResistance.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
-
-
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
 
 public class CollisionResistance {
 
     //declare byte array for digest, inputBytes, tryDigest
     //declare encoding
-    //declare algoorithm
+    //declare algorithm
+    public static byte[] digest;
+    public static String algorithm = "SHA-256";
+    public static String encoding = "UTF-8";
 
 
     public void bruteForce(byte[] digest) {
@@ -21,15 +25,28 @@ public class CollisionResistance {
 
     }
 
-    //public byte[] makeDigest(String inputText){
+    public static byte[] getDigest(String inputText){
 
+        try {
         // create object of message digest with SHA-256
+        MessageDigest msgDig = MessageDigest.getInstance(algorithm);
         // create inputBytes of inputText with correct encoding
         // call object.update with inputBytes
+        msgDig.update(inputText.getBytes(encoding));
         // create digest by object.digest();
+        digest = msgDig.digest();
+        printDigest(digest);
+        } catch(NoSuchAlgorithmException e) {System.out.println("The specified algorithm " + algorithm + " does not exists");}
+        catch(UnsupportedEncodingException ex) {System.out.println("Encoding " + encoding + " not supported");}
+    
+        return digest;
+}
 
-       // return digest;
-   // }
+   public static void printDigest(byte[] digest) {
+       for(int i = 0; i < digest.length; i++)
+            System.out.format("%02x", digest[i]&0xff);
+        System.out.println();
+   }
 
     public static void main(String[] args) {
 
@@ -38,11 +55,8 @@ public class CollisionResistance {
         Scanner sc = new Scanner(System.in);
         String msgToDigest = sc.nextLine();
         System.out.println(msgToDigest);
-        // Make digest by calling function makeDigest
-        Byte[] digest = makeDigest(msgToDigest);
-        //Print digest
-        printDigest(digest);
-        //Perform brute force with digest
+
+        byte[] digest = getDigest(msgToDigest);
         bruteForce(digest);
     }
 }
