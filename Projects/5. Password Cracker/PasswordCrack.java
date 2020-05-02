241332f453e5667eccd38492d9b95b4e13c0c058
Emil Ståhl <emil.stahl@icloud.com>
2020-05-02 12:41:49 +0200

    Compiles

--- Projects/5. Password Cracker/PasswordCrack.java
+++ Projects/5. Password Cracker/PasswordCrack.java
@@ -1,6 +1,14 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PasswordCrack {
 
-    public static void main(String args) {
+    public static void main(String[] args) {
 
+        if (args.length < 2) {
+            System.out.println("Usage: <dictionary> <passwords>");
+            System.exit(1);
+        }
     }
 }
