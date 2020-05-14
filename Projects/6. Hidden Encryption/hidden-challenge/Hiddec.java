e562a3a15359f445085340d0fac189ee4927b7e1
Emil Ståhl <emil.stahl@icloud.com>
2020-05-14 16:52:05 +0200

    hash no errors

--- Projects/6. Hidden Encryption/hidden-challenge/Hiddec.java
+++ Projects/6. Hidden Encryption/hidden-challenge/Hiddec.java
@@ -1,10 +1,22 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.*;
 import java.util.Arrays;
+
+
 
 
 public class Hiddec {
 
     public static byte[] key, ctr, input;
+    public static boolean CTR;
     static String output;
+    static String algorithm = "MD5";
+    public static Cipher cipher;
 
     
     public static void loadArgs(String[] args) {
@@ -24,11 +36,12 @@ public class Hiddec {
                     break;
                 
                 case CTR_FLAG:
+                    CTR = true;
                     ctr = stringToHex(splitted[1]);
                     break;
                 
                 case INPUT_FLAG:
-                    input = stringToHex(splitted[1]);;
+                    input = readFile(splitted[1]);;
                     break;
                 
                 case OUTPUT_FLAG:
@@ -54,7 +67,60 @@ public class Hiddec {
         return b;
     }
 
+    public static byte[] readFile(String input) {
 
+        byte[] byteArray = null;
+        try {
+            byteArray = Files.readAllBytes(Paths.get(input));
+        } 
+        catch(Exception e) {
+            System.out.println("\nAn error occured while reading from file " + input);
+            System.exit(1);
+        }
+        return byteArray;
+    }
+
+    public static byte[] hashKey(byte[] key) throws NoSuchAlgorithmException {
+        
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        System.exit(0);
+        
+        md.update(key);
+        byte[] digest = md.digest();
+
+        return digest;
+    }
+
+    public static void ctr(byte[] key, byte[] encryptedKey, byte[] input, String output) {
+
+        findKey(input, encryptedKey);
+    }
+
+    public static int findKey(byte[] input, byte[] encryptedKey) {
+        for(int i = 0; i <= input.length; i+= 16) {
+            init();
+            byte[] decrypted = cipher.update((Arrays.copyOfRange(input, i, i + 16)));
+            if(Arrays.equals(decrypted,encryptedKey)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void init() {
+
+        try {
+        SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+        IvParameterSpec iv = new IvParameterSpec(ctr);
+        cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
+        }
+        catch (InvalidKeyException exception) {}
+        catch (NoSuchPaddingException ePaddingException) {}
+        catch (NoSuchAlgorithmException eAlgorithmPaddingException) {}
+        catch (InvalidAlgorithmParameterException algorithmParameterException) {}
+        
+    }
 
 
 
@@ -69,5 +135,10 @@ public class Hiddec {
 
         loadArgs(args);
 
+        if(CTR = true) { 
+            try {
+            ctr(key, hashKey(key), input, output);
+            } catch(NoSuchAlgorithmException algorithmException) {}
     }
 } 
+}
