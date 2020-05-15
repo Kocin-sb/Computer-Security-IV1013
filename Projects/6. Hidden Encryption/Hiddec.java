e030d288a4b8e6a60575d3fa67408f1bfb319378
Emil Ståhl <emil.stahl@icloud.com>
2020-05-15 20:14:30 +0200

    task3 works!

--- Projects/6. Hidden Encryption/Hiddec.java
+++ Projects/6. Hidden Encryption/Hiddec.java
@@ -59,14 +59,14 @@ public class Hiddec {
         System.out.println(hiddec.input);
         System.out.println(hiddec.output);
 
-        try {
-            byte[] data = hiddec.findDataECB(stringToHexByteArray(hiddec.key), readFile(hiddec.input), hashKey(stringToHexByteArray(hiddec.key)));
-            writeToFile(data, hiddec.output);
-    
-        }catch(Exception e){
-            throw new Exception(e.getMessage());
-        }    
-}
+        if(hiddec.ctr != null) {
+            byte[] data = hiddec.findDataCTR(stringToHexByteArray(hiddec.key), readFile(hiddec.input), hashKey(stringToHexByteArray(hiddec.key)), stringToHexByteArray(hiddec.ctr));            
+            writeToFile(data, hiddec.output); 
+        }
+
+        byte[] data = hiddec.findDataECB(stringToHexByteArray(hiddec.key), readFile(hiddec.input), hashKey(stringToHexByteArray(hiddec.key)));
+        writeToFile(data, hiddec.output); 
+    }
 
     public static byte[] hashKey(byte[] key) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("MD5");
@@ -104,6 +104,33 @@ public class Hiddec {
         } catch (IOException e) {
           throw new IOException(String.format("Couldn't write data to the file: \"%s\"", outputFileName));
         }
+    }
+    public byte[] findDataCTR(byte[] key, byte[] input, byte[] hash, byte[] ctr) throws Exception{
+        byte[] encData = {};
+        for(int i=0; i<input.length; i+=16){
+            encData = decryptCTR(key, ctr, Arrays.copyOfRange(input,i,input.length));
+            if(testBlob(encData, hash)){
+                break;
+            }
+        }
+        if(!testBlob(encData, hash)){
+            throw new Exception("Could not find blob");
+        }
+        for(int i=hash.length; i<encData.length; i++){
+            if(testBlob(encData,i, hash)){
+                byte[] foundData = Arrays.copyOfRange(encData,hash.length,i);
+                int start = i;
+                start += hash.length;
+                byte[] validationData = Arrays.copyOfRange(encData,start,start+hash.length);
+                if(validate(MessageDigest.getInstance("MD5").digest(foundData),validationData)){
+                    return foundData;
+                }
+                else{
+                    throw new Exception("Data could not be validated");
+                }
+            }
+        }
+        throw new Exception("Data could not be found in the blob");
     }
 
     public byte[] findDataECB(byte[] key, byte[] input, byte[] hash) throws Exception{
