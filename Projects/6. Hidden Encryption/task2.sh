8e88597f5c6aac9259b7c5135858fd89cae6354c
Emil Ståhl <emil.stahl@icloud.com>
2020-05-17 01:07:30 +0200

    created badass shell script

--- Projects/6. Hidden Encryption/task2.sh
+++ Projects/6. Hidden Encryption/task2.sh
@@ -1,4 +1,22 @@
 clear
+
+echo "Encrypting task2.data:"
+echo " "
+sleep 1
+
 rm Hiddec.class file2.data
 javac Hidenc.java
 java Hidenc --key=83d9174ba6ea9d2763ce045bd40521d6 --input=task2.data --output=file2.data --offset=80
+
+echo " "
+echo "Decrypting file2.data:"
+echo " "
+sleep 1
+
+rm Hiddec.class file2.txt
+javac Hiddec.java
+java Hiddec --key=83d9174ba6ea9d2763ce045bd40521d6 --input=file2.data --output=file2.txt 
+
+echo " "
+cat file2.txt
+echo " "
