50f7e337e458c4b9ad18085138d1aa95c29a32c8
Emil Ståhl <emil.stahl@icloud.com>
2020-05-16 22:47:48 +0200

    Added offset

--- Projects/6. Hidden Encryption/task2.sh
+++ Projects/6. Hidden Encryption/task2.sh
@@ -1,4 +1,4 @@
 clear
-rm Hiddec.class file1.txt
-javac Hiddec.java
-java Hiddec --key=83d9174ba6ea9d2763ce045bd40521d6 --input=task2.data --output=file2.data.txt 
+rm Hiddec.class file2.data
+javac Hidenc.java
+java Hidenc --key=83d9174ba6ea9d2763ce045bd40521d6 --input=task2.data --output=file2.data --offset=80
