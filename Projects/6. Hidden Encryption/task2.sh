bae95e90393530186929a7401b1e3d63213a5eed
Emil Ståhl <emil.stahl@icloud.com>
2020-05-17 20:41:22 +0200

    Update task2.sh

--- Projects/6. Hidden Encryption/task2.sh
+++ Projects/6. Hidden Encryption/task2.sh
@@ -4,7 +4,7 @@ echo "Encrypting task2.data:"
 echo " "
 sleep 1
 
-rm Hiddec.class file2.data
+rm Hidenc.class file2.data
 javac Hidenc.java
 java Hidenc --key=83d9174ba6ea9d2763ce045bd40521d6 --input=task2.data --output=file2.data --offset=80
 
