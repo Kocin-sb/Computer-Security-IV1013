993c71b4c4742ba7a866d4b8d96aa40499a9cc3f
Emil Ståhl <emil.stahl@icloud.com>
2020-05-18 16:26:19 +0200

    specifying size for task2

--- Projects/6. Hidden Encryption/task2.sh
+++ Projects/6. Hidden Encryption/task2.sh
@@ -6,7 +6,7 @@ sleep 1
 
 rm Hidenc.class file2.data
 javac Hidenc.java
-java Hidenc --key=83d9174ba6ea9d2763ce045bd40521d6 --input=task2.data --output=file2.data --offset=80
+java Hidenc --key=83d9174ba6ea9d2763ce045bd40521d6 --input=task2.data --output=file2.data --offset=80 --size=2048
 
 echo " "
 echo "Decrypting file2.data:"
